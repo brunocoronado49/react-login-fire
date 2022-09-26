e1f4771c937a05ad658e15b97569cf75eaf439ed
Bruno Coronado <brunocoronado@MacBook-Air-de-Bruno.local>
2022-09-26 12:24:05 -0500

    agregando componentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path="/" />
+    </Routes>
   )
 }
 
