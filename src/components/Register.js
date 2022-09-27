4bb260080b2999fd84de01271dee03234b6f575b
Bruno Coronado <brunocoronado@MacBook-Air-de-Bruno.local>
2022-09-27 00:48:17 -0500

    Agregando firebase auth para registro de correos

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,40 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../context/authContext'
 
 export const Register = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  })
+
+  const navigate = useNavigate()
+
+  const { signUp } = useAuth()
+
+  const onChange = ({ target: { name, value } }) => {
+    setUser({ ...user, [name]: value })
+  }
+
+  const onSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      await signUp(user.email, user.password)
+      navigate("/")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div>Register</div>
+    <div>
+      <form onSubmit={onSubmit}>
+        <label>Email:</label>
+        <input type="email" name="email" onChange={onChange} />
+        <label>Password:</label>
+        <input type="password" name="password" onChange={onChange} />
+        <button>Register</button>
+      </form>
+    </div>
   )
 }
-
--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,4 +1,6 @@
 import { createContext, useContext } from 'react'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../config/firebase'
 
 export const authContext = createContext()
 
@@ -9,11 +11,12 @@ export const useAuth = () => {
 }
 
 export function AuthProvider({ children }) {
-  const user = {
-    login: true,
+  const signUp = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
   }
+
   return (
-    <authContext.Provider value={user}>
+    <authContext.Provider value={{signUp}}>
       {children}
     </authContext.Provider>
   )
